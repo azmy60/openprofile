6c30b31c99d186cbf28e4c3034dab66ed68403d6
azmy60 <38707148+azmy60@users.noreply.github.com>
2023-07-17 12:48:01 +0700

    Add icons

--- app/builder.tsx
+++ app/builder.tsx
@@ -18,6 +18,8 @@ import ReactPDF, {
   Text,
   View,
   PDFDownloadLink,
+  Svg,
+  Path,
 } from "@react-pdf/renderer";
 import { marked } from "marked";
 import { useAtom, useAtomValue } from "jotai";
@@ -34,6 +36,9 @@ import {
   PencilSquareIcon,
   XMarkIcon,
   CheckIcon,
+  MapPinIcon,
+  EnvelopeIcon,
+  PhoneIcon,
 } from "@heroicons/react/24/outline";
 import { moveArrayElement } from "./helpers";
 
@@ -104,7 +109,7 @@ export default function Builder() {
               onChange={onChange}
             />
             <SimpleInput
-              label="Tel"
+              label="Telephone"
               name="tel"
               value={content.tel}
               onChange={onChange}
@@ -133,7 +138,7 @@ export default function Builder() {
               iframeRef={(el) => updateScale(el, pdfViewer)}
               className="origin-top-left"
             >
-              <Doc />
+              <Doc usingCustomPDFViewer />
             </CustomDynamicPDFViewer>
           </div>
         </div>
@@ -634,7 +639,7 @@ const Link: React.FC<React.PropsWithChildren<ReactPDF.LinkProps>> = (props) => (
   <ReactPDFLink {...props} style={{ display: "flex", ...props.style }} />
 );
 
-const Doc: React.FC = () => {
+const Doc: React.FC<{ usingCustomPDFViewer?: boolean }> = (props) => {
   const { name, email, tel, location, sections } = useAtomValue(contentAtom);
   return (
     <Document>
@@ -666,11 +671,94 @@ const Doc: React.FC = () => {
               {name}
             </Text>
             <View style={{ display: "flex", flexDirection: "column", gap: 4 }}>
-              <Text>{location}</Text>
-              <Link src={`mailto:${email}`} style={{ display: "flex" }}>
-                {email}
-              </Link>
-              <Text>{tel}</Text>
+              {location && (
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4pt",
+                  }}
+                >
+                  {props.usingCustomPDFViewer ? (
+                    <MapPinIcon style={{ width: "12pt", height: "12pt" }} />
+                  ) : (
+                    <Svg
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      style={{ width: "12pt", height: "12pt" }}
+                    >
+                      <Path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <Path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                      />
+                    </Svg>
+                  )}
+                  <Text>{location}</Text>
+                </View>
+              )}
+              {email && (
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4pt",
+                  }}
+                >
+                  {props.usingCustomPDFViewer ? (
+                    <EnvelopeIcon style={{ width: "12pt", height: "12pt" }} />
+                  ) : (
+                    <Svg
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      style={{ width: "12pt", height: "12pt" }}
+                    >
+                      <Path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                      />
+                    </Svg>
+                  )}
+                  <Link src={`mailto:${email}`} style={{ display: "flex" }}>
+                    {email}
+                  </Link>
+                </View>
+              )}
+              {tel && (
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4pt",
+                  }}
+                >
+                  {props.usingCustomPDFViewer ? (
+                    <PhoneIcon style={{ width: "12pt", height: "12pt" }} />
+                  ) : (
+                    <Svg
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      style={{ width: "12pt", height: "12pt" }}
+                    >
+                      <Path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                      />
+                    </Svg>
+                  )}
+                  <Text>{tel}</Text>
+                </View>
+              )}
             </View>
           </View>
           {sections.map((section, sectionIndex) => (
